ecc6f1525c872741e3c9a2f090cbe135fc94aae9
Marat Abdulin <esend.work@gmail.com>
2015-09-12 23:46:10 +0300

    Update hours working

--- src/vk_analyzer/posts_analyzer.clj
+++ src/vk_analyzer/posts_analyzer.clj
@@ -1,15 +1,20 @@
 (ns vk-analyzer.posts-analyzer
 	(:require [clj-time.coerce :as coerce]
-			  [clj-time.format :as formatter]))
+			  [clj-time.format :as formatter]
+			  [clj-time.core :as time]))
 
 (def hour-formatter (formatter/formatter "HH"))
 
 (defn get-hour [timestamp] 
 	;  Учитывать временную зону
 	(formatter/unparse hour-formatter 
-		(coerce/from-long (* timestamp 1000))))
+		(time/from-time-zone 
+			(coerce/from-long (* timestamp 1000))
+			(time/time-zone-for-offset -3))
+		))
 
 (defn transform-time [value]
+	(println (get-hour (value 0)) )
 	[(get-hour (value 0)) 
 		(value 1)])
 
@@ -17,27 +22,22 @@
   [numbers]
   (/ (apply + numbers) (count numbers)))
 
-(defn get-likes [inpu]
-	[(inpu 0)
-	(vec (map second (inpu 1)))])
-
-(defn grouped [params]
+(defn group-by-hour [params]
 	(group-by first (map transform-time params)))
 
-(defn genefucker [elem]
+(defn get-averaged-likes [elem]
 	[(elem 0) (average (map second (elem 1)))])
 
-(defn analyze [params]
-	(println (into (sorted-map) (vec (map genefucker (grouped params)))))
-	(into (sorted-map) (vec (map genefucker (grouped params)))))
+(defn get-hours-likes [params]
+	(into 
+		(sorted-map) 
+		(vec (map get-averaged-likes (group-by-hour params)))))
 
 (defn get-hours-array [params]
-	(let [hours (analyze params)]
-		(map-indexed (fn [index, item] (if (hours (str index))
-			(hours (str index))
-			0
-			) ) (vec (range 24)))))
-
-; (defn evaluate-likes-count "should return array of results by hour" []
-
-  ; )
+	(let [hours (get-hours-likes params)] ; Получаем часы со средними лайками {"12" 245, "23" 542}
+		(map-indexed  ; собираем новый массив с индексами
+			(fn [index, item] 
+				(if (hours (str index)) ; Если в часах есть лайки
+					(hours (str index)) ; добавляем их
+					0)) ; иначе просто 0
+			(vec (range 24)))))
