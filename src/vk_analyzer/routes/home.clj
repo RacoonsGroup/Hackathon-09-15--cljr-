c4c53580cdfbe1dc8bc5deefc28acbf030e18817
Marat Abdulin <esend.work@gmail.com>
2015-09-12 16:12:34 +0300

    Add simple form with post action

--- src/vk_analyzer/routes/home.clj
+++ src/vk_analyzer/routes/home.clj
@@ -1,6 +1,6 @@
 (ns vk-analyzer.routes.home
   (:require [vk-analyzer.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
             [clojure.java.io :as io]))
 
@@ -11,7 +11,12 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn about-post [{:keys [firstname secondname]}]
+	(println firstname secondname)
+  	(layout/render "about-post.html" {:firstname firstname :secondname secondname}))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (POST "/about" {params :params} (about-post params)))
 
