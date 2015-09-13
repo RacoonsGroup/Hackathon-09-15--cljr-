c7bffc9314c508b2fae75e9685a176c55c488c45
Marat Abdulin <esend.work@gmail.com>
2015-09-13 09:18:55 +0300

    Decreased requests

--- src/vk_analyzer/routes/home.clj
+++ src/vk_analyzer/routes/home.clj
@@ -14,7 +14,7 @@
   (layout/render "about.html"))
 
 (defn analyze-likes [{:keys [id domain keywords]}]
-    (let [api-data (vk-api/get-wall {:owner_id id :domain domain :count 500})]
+    (let [api-data (vk-api/get-wall {:owner_id id :domain domain :count 200})]
   	(layout/render "analyze-likes.html" {:data (charts/analyze-data api-data) :scatter-data (charts/analyze-data-for-scatter api-data)})))
 
 (defroutes home-routes
